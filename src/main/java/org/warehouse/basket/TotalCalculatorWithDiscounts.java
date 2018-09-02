5c1324bf5f903bcbe7a1dfefd01d2e9adb301c6f
Flickster <christophflick@gmx.de>
2018-09-02 21:48:52 +0200

    Refactored calculation

--- src/main/java/org/warehouse/basket/TotalCalculatorWithDiscounts.java
+++ src/main/java/org/warehouse/basket/TotalCalculatorWithDiscounts.java
@@ -1,10 +1,12 @@
 package org.warehouse.basket;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toUnmodifiableSet;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 import org.warehouse.discount.Discount;
 import org.warehouse.discount.DiscountTypes;
@@ -18,18 +20,30 @@ public class TotalCalculatorWithDiscounts implements TotalCalculator {
 		Map<String, Set<DiscountTypes>> discountsByProductId = basket.getDiscountsByProductId();
 
 		discountsByProductId.entrySet().stream().forEach(entry -> {
-			List<Product> viableProducts = products.stream()
-					.filter(p -> p.getId().equals(entry.getKey()))
-					.collect(Collectors.toList());
-			Set<Discount> discounts = entry.getValue().stream()
-					.map(DiscountTypes::getActualDiscount)
-					.collect(Collectors.toUnmodifiableSet());
-			discounts.forEach(applyDiscountTo(viableProducts));
+			List<Product> viableProducts = getProductsWithProductId(products, entry.getKey());
+			Set<Discount> discounts = getDiscountsForDiscountTypes(entry.getValue());
+			applyDiscountsOnProducts(discounts, viableProducts);
 		});
 
 		return products.stream()
 				.mapToLong(Product::getPriceInCentsAfterDiscount)
 				.sum();
+	}
+
+	private Set<Discount> getDiscountsForDiscountTypes(Set<DiscountTypes> discuntTypes) {
+		return discuntTypes.stream()
+				.map(DiscountTypes::getActualDiscount)
+				.collect(toUnmodifiableSet());
+	}
+
+	private List<Product> getProductsWithProductId(List<Product> products, String productId) {
+		return products.stream()
+				.filter(p -> p.getId().equals(productId))
+				.collect(toList());
+	}
+	
+	private void applyDiscountsOnProducts(Set<Discount> discounts, List<Product> viableProducts) {
+		discounts.forEach(applyDiscountTo(viableProducts));
 	}
 
 	private Consumer<? super Discount> applyDiscountTo(List<Product> viableProducts) {
